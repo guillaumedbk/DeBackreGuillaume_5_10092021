d639b2fb7a0f77e5cf09305fd68f54823f580889
Guillaume De Backre <debackre.guillaume@gmail.com>
2021-09-13 16:35:30 +0200

    Récupération et Affichage
    Affichage des données d'un produit, avec l'id

--- Front-end/accueil.js
+++ Front-end/accueil.js
@@ -10,9 +10,18 @@ fetch("http://localhost:3000/api/teddies")
     .catch(function(err) {
         // Une erreur est survenue
     });
+
+let description_card= document.getElementById("description");
+let titre_card= document.getElementById("titre");
+let image=document.getElementById('image_card')
+
+fetch("http://localhost:3000/api/teddies/5be9c8541c9d440000665243")
+    .then(reponse => reponse.json())
+    .then(data => {
+        description_card.textContent=data.description;
+        titre_card.textContent=data.name;
+        image.src=data.imageUrl;
+    }
+        
+        )
     
-  
-
-
-
-
