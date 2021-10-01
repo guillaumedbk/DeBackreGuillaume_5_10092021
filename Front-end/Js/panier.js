19ec23d48981a5250b8b1a4bdebe46cf362dab03
Guillaume <debackre.guillaume@gmail.com>
2021-10-01 17:18:34 +0200

    Nettoyage codepen
    Commentaires

--- Front-end/Js/panier.js
+++ Front-end/Js/panier.js
@@ -1,189 +1,216 @@
-//Nombre de produits dans le panier
-function nombrePanier(produit){
-    
-    let produitSession = localStorage.getItem('nombrePanier');
-    localStorage.setItem('nombrePanier', 1);
-    produitSession = parseInt(produitSession);
-    
-    if(produitSession){
-        localStorage.setItem('nombrePanier', produitSession + 1);
-        document.querySelector('#span_panier').textContent = produitSession + 1;
-    }else{
-        localStorage.setItem('nombreProduits', 1);
-        document.querySelector('#span_panier').textContent= 1;
-    } 
-}
-//Empecher la réiniatialisation du nombre de pdts dans le panier au chargement
-function auChargement(){
-    let produitSession = localStorage.getItem('nombrePanier');
-
-    if(produitSession){
-        document.querySelector('#span_panier').textContent = produitSession;
-        
-    }
-}
-auChargement();
-
-
-//////////////METTRE DANS UNE FONCTION/////////////////
- 
-//Récupération des produits du panier pour afficher
-let monPanier = JSON.parse(localStorage.getItem("panierClient"));
-console.log(monPanier)
-
-let articlesNorbert='5be9c8541c9d440000665243';
-let articlesArnold='5beaa8bf1c9d440000a57d94';
-let articlesLenny='5beaaa8f1c9d440000a57d95';
-let articlesGustav= '5beaabe91c9d440000a57d96';
-let articlesGarfunkel='5beaacd41c9d440000a57d97';
-
-let lesArticlesNorbert=0;
-let lesArticlesArnold=0;
-let lesArticlesLenny=0;
-let lesArticlesGustav=0;
-let lesArticlesGarfunkel=0;
-
-fetch("http://localhost:3000/api/teddies")
+//Connexion base de données
+function fetchApi (){
+    fetch("http://localhost:3000/api/teddies")
     .then(function(reponse) {
         if (reponse.ok) {
         return reponse.json();
         }
     })
     .then(function(data) {
-console.log(data)
+        let containerPanier = document.getElementById("container_panier");
+                containerPanier.innerHTML = ``
+        console.log(data)
+        afficheLePanier(data);
+        calculatePrice(data);
+        nombrePanier();
+    })
+    .catch(function(err) {
+        // Une erreur est survenue
+    });
+    }
+    
+fetchApi();
 
-        //Quantité de produits
-        for (produits of monPanier){
-        
-            if(produits == articlesNorbert){
-                lesArticlesNorbert++;
+//RECUPERATION ET AFFICHAGE DU PANIER
+function afficheLePanier(data){
+    total=[];
+    let monPanier = JSON.parse(localStorage.getItem("panierClient"));
+    
+    //VERIFIER SI LE PANIER EST VIDE OU NON
+    if(monPanier == ""){
+        //MENTIONNER QUE LE PANIER EST VIDE
+        let main = document.getElementById('main');
+        main.innerHTML=`<h2>Votre panier est vide</h2>`
+        let formulaire = document.getElementById('formulaire');
+        formulaire.innerHTML=``;
+    }else{
+        //AFFICHER LES DONNEES DU PANIER
+        for (let teddy of data) {
+            //BOUCLER SUR LE PANIER ET AFFICHER LES ELEMENTS CORRESPONDANT
+            if(monPanier.includes(teddy._id)){
+                let containerPanier = document.getElementById("container_panier");
+                containerPanier.innerHTML += `
+                <tr class="table-light"> 
+                <td> <button class="remove btn btn-danger" id="${teddy._id}" type="button"><i class="fas fa-minus-circle"></i></button></td>
+                    <td>Ours : ${teddy.name} </td>
+                    <td><img src="${teddy.imageUrl}" width="100px"> </img></td>
+                    <td>Quantité : <input type="number" id="${teddy._id}" value="${countTeddies(teddy._id)}" class="listenQte"></input></td>
+                    <td>${(teddy.price/100)*countTeddies(teddy._id)}€</td>
+                </tr>
+                
+            `
             }
-            if(produits == articlesArnold){
-                lesArticlesArnold++;
-            }
-            if(produits == articlesLenny){
-                lesArticlesLenny++;
-            }
-            if(produits == articlesGustav){
-                lesArticlesGustav++;
-            }
-            if(produits == articlesGarfunkel){
-                lesArticlesGarfunkel++;
-            }
-        }
-        
-        //////////////AFFICHER LE RECAPITULATIF DE COMMANDE/////////////////
-        let containerPanier = document.getElementById('container_panier');
-        let totalDuPanier = document.getElementById('total_panier');
+            //Fonction pour la suppression des éléments au clic
+            remove()
+            //Fonction pour écouter la quantité de l'input
+            changeQte() 
+    }
+    } 
+}
 
-    let totalNorbert = lesArticlesNorbert*29;
-    let totalArnold = lesArticlesArnold*39;
-    let totalLenny = lesArticlesLenny*59;
-    let totalGustav = lesArticlesGustav*45;
-    let totalGarfunkel = lesArticlesGarfunkel*55;
+//CREATION DU PANIER DANS LE LOCAL STORAGE 
+function ajoutPanier(){
+    let initPanier = JSON.parse(localStorage.getItem("panierClient"));
+        if(localStorage.getItem('panierClient')){
+            //Si present - ajout des données au panier
+            initPanier.push(data_id);
+            localStorage.setItem("panierClient", JSON.stringify(initPanier));
+        }else{
+            //Si vide - initialisation du panier dans le local storage
+            let initPanier = [];
+            localStorage.setItem("panierClient", JSON.stringify(initPanier));
+        }
+}
 
-    let totalPanier =0;
+//Tableau vide pour stocker les prix des produits du panier
+let total = [];
 
-        if(lesArticlesNorbert > 0){
-            totalPanier += totalNorbert;
-            containerPanier.innerHTML += `
-         
-            <tr class="table-light"> 
-                <td> <button class="remove btn btn-danger" type="button"><i class="fas fa-minus-circle"></i></button></td>
-                <td>Ours Norbert : </td>
-                <td><input type="number">${lesArticlesNorbert}</input></td>
-                <td>${totalNorbert}€</td>
-            </tr>
-           `
-        
+//Ajoute les prix de chaque élément du panier dans le tableau
+function calculatePrice(data){
+    let monPanier = JSON.parse(localStorage.getItem("panierClient"));
+    for(ours of data){
+        for(element of monPanier){
+            //POUR LES ELEMENTS PRESENTS DANS LE PANIER, AJOUTER LEUR PRIX DANS LE TABLEAU
+            if(ours._id == element){
+                total.push(ours.price/100)
+            }
         }
-        if(lesArticlesArnold > 0){
-            totalPanier += totalArnold;
-            containerPanier.innerHTML += `
-           
-           <tr class="table-light"> 
-                <td> <button id="remove" class="remove btn btn-danger" type="button"><i class="fas fa-minus-circle"></i></button></td>
-                <td>Ours Arnold : </td>
-                <td><input type="number" value="${lesArticlesArnold}" class="changeArnold"></input></td>
-                <td>${totalArnold}€</td>
+    }
+   //Appel de la fonction pour afficher le prix total calculé par la fonction somme
+    showPrice()
+}
+
+function showPrice(){
+    //Affichage panier
+    let totalPanier = document.getElementById('total_panier');
+    //Ligne pour le total du panier
+    totalPanier.innerHTML = `
+            <tr class="table-success"> 
+                        <td> Prix total:</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>${somme()}€</td>
             </tr>
-           `
-           
-        }
-        if(lesArticlesLenny > 0){
-            totalPanier += totalLenny;
-            containerPanier.innerHTML += `
-           
-           <tr class="table-light"> 
-                <td> <button class="remove btn btn-danger" type="button"><i class="fas fa-minus-circle"></i></button></td>
-                <td>Ours Lenny : </td>
-                <td>${lesArticlesLenny} </td>
-                <td>${totalLenny}€</td>
-            </tr>
-           `
-        
-        }
-        if(lesArticlesGustav > 0){
-            totalPanier += totalGustav;
-            containerPanier.innerHTML += `
-           
-           <tr class="table-light"> 
-                <td> <button class="remove btn btn-danger" type="button"><i class="fas fa-minus-circle"></i></button></td>
-                <td>Ours Gustav : </td>
-                <td>${lesArticlesGustav} </td>
-                <td>${totalGustav}€</td>
-            </tr>
-           `
-        }
-        if(lesArticlesGarfunkel > 0){
-            totalPanier += totalGarfunkel;
-            containerPanier.innerHTML += `
-           
-           <tr class="table-light"> 
-                <td> <button class="remove btn btn-danger" type="button"><i class="fas fa-minus-circle"></i></button></td>
-                <td>Ours Garfunkel: </td>
-                <td>${lesArticlesGarfunkel} </td>
-                <td>${totalGarfunkel}€</td>
-            </tr>
-           `
-        }
-        totalDuPanier.innerHTML=`
-        <tr class="table-success"> 
-            <td></td>
-            <td>Prix total : </td>
-            <td></td>
-            <td>${totalPanier}€</td>
-        </tr>
         `
-//let tableau = document.getElementById('container_panier');
-//tableau.deleteRow(1);
+}
+//Calcul du prix total
+function somme(){
+    let prix=0;
+    //Additionne chaque élément du tableau l'un après l'autre
+    for(el of total){
+        prix+=el;
+    }
+    //Retour le montant total
+    return(prix)
+}
 
-function removeQte(){
-    let boutonRemove = document.querySelector('.remove');
-    let produitSession = localStorage.getItem('nombrePanier');
-    console.log(produitSession);
-    boutonRemove.addEventListener('click', () => {
-        lesArticlesArnold-=1;
-        produitSession-=1;
-        console.log(produitSession);
-        //Valeur de l'input
-        let changeArnold = document.querySelector('.changeArnold');
-        changeArnold.value = lesArticlesArnold;
-        //Valeur de l'icone panier
-        document.querySelector('#span_panier').textContent= produitSession;
-        //Suprrimer un article en question du localstorage
-        let panierStorage = localStorage.getItem('panierClient');
-        
+//FONCTION QUI ECOUTE L'INPUT QTE ET AJUSTE LES ELEMENTS
+function  changeQte(){
+    let inputQte = document.querySelectorAll('.listenQte'); 
+    let monPanier = JSON.parse(localStorage.getItem("panierClient"));
+   
+    inputQte.forEach(element => {
+        element.addEventListener('change', () =>{
+            //Afficher avec les nouvelles valeurs
+            fetchApi()
+            
+            newQte = element.value;   //Nouvelle valeur de l'input
+            let teddyid = element.getAttribute("id"); //Recupère l'id de l'ours en question
+            let panierSansOursEnQuestion = monPanier.filter(el => el != teddyid); //Panier sans les ours en question 
+              
+            for(i=0;i<newQte;i++){
+                panierSansOursEnQuestion.push(teddyid); //Ajoute la nouvelle quantité de l'ours en question 
+            }
+            localStorage.setItem('panierClient', JSON.stringify(panierSansOursEnQuestion)); //Panier à jour dans le localstorage
+            
+        })
     })
 }
-removeQte();
 
-    });   
+//SUPPRESSION DES ELEMENTS AU CLIC
+function remove(){
+    let boutonRemove = document.querySelectorAll('.remove');
+    let monPanier = JSON.parse(localStorage.getItem("panierClient"));
+    boutonRemove.forEach(element => {
+        element.addEventListener('click', () => {
+            let teddyid = element.getAttribute("id"); //Récupère l'id de l'ours en question 
+                let filtre = monPanier.filter(item => item != teddyid); //Renvoi le tableau (monPanier) sans les ours en question 
 
+                localStorage.setItem('panierClient', JSON.stringify(filtre)) //Ajouter ce nouveau tableau au localstorage
+                fetchApi(); //Appel de la fonction pour afficher avec les valeurs à jour
+        })
+    });
+}
 
+//COMPTAGE DES ELEMENTS
+function countTeddies(id){
+    let monPanier = JSON.parse(localStorage.getItem("panierClient"));
 
+    let qteTeddies = {}; 
+    for(let i=0; i<monPanier.length; i++){
+        let iteration = monPanier[i];
+        qteTeddies[iteration] = qteTeddies[iteration] ? qteTeddies[iteration] +1 :1;
+    }
+    //return(qteTeddies.id);
+    return(qteTeddies[id])
+}
 
+//NOMBRE D'ARTICLES DANS LE PANIER
+function nombrePanier(){
+    let produitSession1 = JSON.parse(localStorage.getItem('panierClient'));
+    let produitSession2 = produitSession1.length;
+        document.querySelector('#span_panier').textContent = produitSession2;  
+}
 
+// Récupération Formulaire
+const getFormFirstName = document.getElementById("inputFirstName");
+const getFormName = document.getElementById("inputName");
+const getFormEmail = document.getElementById("inputEmail");
+const getFormAdress1 = document.getElementById("inputAddress");
+const getFormCity = document.getElementById("inputCity");
+const getSubmitButton = document.getElementById("submitButton");
 
+//Envoie des données du formulaire 
+function sendToApi(){
+    let contact = 
+    {
+        firstName: getFormFirstName.value,
+        lastName: getFormName.value,
+        address: getFormAdress1.value,
+        city : getFormCity.value,
+        email: getFormEmail.value,
+    }
+    let products=JSON.parse(localStorage.getItem("panierClient"));
 
+    let body = {contact, products} //Objet avec tous les éléments
 
+    fetch("http://localhost:3000/api/teddies/order", {
+        method: 'POST',
+        headers: { 
+        'Accept': 'application/json', 
+        'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(body) 
+    })
+   .then(response => response.json())
+    .then((response) => {
+     console.log(response)
+     localStorage.setItem("orderConfirmation", JSON.stringify(response)); //Envoi la réponse de l'api dans le locastorage
+     window.location="./confirmation.html" //Renvoi à la page confirmation de commande
+     })
+      .catch(function(error) {
+        alert('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+      });
+ }
+ 
--- Front-end/Js/produit.js
+++ Front-end/Js/produit.js
@@ -1,11 +1,15 @@
+//ID DU PRODUIT
 const idDuProduit = getArticleId();
+//URL AVEC L'ID DU PRODUIT EN QUESTION
 const urlProduit = "http://localhost:3000/api/teddies".concat('/', idDuProduit);
 let panier = localStorage.getItem("panierClient");
 
+//Récupérer l'id de l'article
 function getArticleId(){
     return new URL(location.href).searchParams.get("id");
 }
-
+getArticle(idDuProduit);
+//Fonction principale qui fetch et affiche les produits
 function getArticle(){
     return fetch(urlProduit)
     .then(function(reponse) {
@@ -14,64 +18,30 @@ function getArticle(){
        }
    })
    .then(function(data) {
-       //AJOUT AU PANIER
-       
+       //Affichage du contenu de la page produit
+       afficheProduit(data) 
+       //Affichage du span panier avec le nombre d'article dans le panier
+        nombrePanier();
+       //Ajout au panier au clic 
        bouton.addEventListener('click', () => {
-        let initPanier = JSON.parse(localStorage.getItem("panierClient"));
-        
-        let idPanier = JSON.parse(localStorage.getItem(data._id))
-        //Vérifier si le panier existe déjà
-        if(localStorage.getItem('panierClient')){
-            //Pousser les éléments dans le tableau initPanier qui comprend tous les produits
-            initPanier.push(data._id);
-            localStorage.setItem("panierClient", JSON.stringify(initPanier));
-
-        //Si il n'existe pas, initialisation du panier
-        }else{
-            //Tableau qui comprendra tous les objets
-            let initPanier = [];
-            initPanier.push(data._id);
-            localStorage.setItem("panierClient", JSON.stringify(initPanier));
-        }
-        auChargement2();
-       })
-       
-       
-       let nomProduit = data.name;
-       let descriptionProduit = data.description;
-       let imageProduit = data.imageUrl;
-       let prixProduit = data.price;
-       
-       let titre = document.getElementById('titre').textContent= "Salut, moi c'est " .concat(nomProduit, " !");
-       let titreProduit = document.getElementById('nomProduit').textContent= nomProduit;
-       let description = document.getElementById('description').textContent= descriptionProduit;
-       let image = document.getElementById('image').src= imageProduit;
-       let prix = document.getElementById('prix').textContent= "".concat(prixProduit/100, "€");
-
-        for(let colori of data.colors){
-            document.getElementById('listeUl').innerHTML += ` <li>${colori}</li>  `
-           }
-       
+        ajoutPanier(data)
+       })   
    })
    .catch(function(err) {
        // Une erreur est survenue
    });
 }
 
-getArticle(idDuProduit);
-
-
-//NOMBRE D'AJOUT AU PANIER
+//NOMBRE D'ARTICLE DANS LE PANIER
 let bouton = document.querySelector('#ajout_panier');
-
-function auChargement2(){
+function nombrePanier(){
     let produitSession1 = JSON.parse(localStorage.getItem('panierClient'));
     let produitSession2 = produitSession1.length;
         document.querySelector('#span_panier').textContent = produitSession2;  
 }
 
-function ajoutPanier(){
-    bouton.addEventListener('click', () => {
+//AJOUT AU PANIER
+function ajoutPanier(data){
         let initPanier = JSON.parse(localStorage.getItem("panierClient"));
         
         let idPanier = JSON.parse(localStorage.getItem(data._id))
@@ -88,4 +58,21 @@ function ajoutPanier(){
             initPanier.push(data._id);
             localStorage.setItem("panierClient", JSON.stringify(initPanier));
         }
-    })}
+    }
+//AFFICHAGE DES DONNEES DANS LA PAGE PRODUIT 
+function afficheProduit(data){
+    let nomProduit = data.name;
+    let descriptionProduit = data.description;
+    let imageProduit = data.imageUrl;
+    let prixProduit = data.price;
+    
+    let titre = document.getElementById('titre').textContent= "Salut, moi c'est " .concat(nomProduit, " !");
+    let titreProduit = document.getElementById('nomProduit').textContent= nomProduit;
+    let description = document.getElementById('description').textContent= descriptionProduit;
+    let image = document.getElementById('image').src= imageProduit;
+    let prix = document.getElementById('prix').textContent= "".concat(prixProduit/100, "€");
+
+     for(let colori of data.colors){
+         document.getElementById('listeUl').innerHTML += ` <li>${colori}</li>  `
+        }
+}
